4391ba9ea5da8222c5e9ad7d7b78c462945eef78
Manuel Sabogal <mfer32@gmail.com>
2016-01-28 19:39:04 -0500

    Code refactoring, rotation added.

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,11 +1,16 @@
 ﻿using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.Foundation;
 
 namespace Surface
 {
     public sealed partial class MainPage : Page
     {
+        Point startTranslation;
+        double startRotation;
+        double startScale;
+
         public MainPage()
         {
             this.InitializeComponent();
@@ -13,7 +18,13 @@ namespace Surface
 
         private void ImageOnManipulationStarted(object sender, ManipulationStartedRoutedEventArgs e)
         {
-            ((Image)sender).Opacity = 0.4;
+            var image = (Image)sender;
+            image.Opacity = 0.4;
+
+            var transform = (CompositeTransform)image.RenderTransform;
+            startTranslation = new Point(transform.TranslateX, transform.TranslateY);
+            startRotation = transform.Rotation;
+            startScale = transform.ScaleX;
         }
 
         private void ImageOnManipulationDelta(object sender, ManipulationDeltaRoutedEventArgs e)
@@ -21,67 +32,11 @@ namespace Surface
             var image = (Image)sender;
             var transform = (CompositeTransform)image.RenderTransform;
 
-            var imageWidth = image.ActualWidth;
-            var imageHeight = image.ActualHeight;
-            var deltaScale = e.Delta.Scale;
-            var deltaTranslationX = e.Delta.Translation.X;
-            var deltaTranslationY = e.Delta.Translation.Y;
-
-
-            if (imageHeight * (transform.ScaleY * deltaScale) < DrawingArea.ActualHeight && imageWidth * (transform.ScaleX * deltaScale) < DrawingArea.ActualWidth)
-            {
-                transform.ScaleX *= deltaScale;
-                transform.ScaleY *= deltaScale;
-            }
-
-            if (deltaTranslationX < 0) // Izquierda
-            {
-                if (DrawingArea.ActualWidth / 2 + (transform.TranslateX + deltaTranslationX) - imageWidth / 2 > 0)
-                {
-                    transform.TranslateX += deltaTranslationX;
-                }
-                else
-                {
-                    transform.TranslateX = imageWidth / 2 - DrawingArea.ActualWidth / 2;
-                }
-            }
-            else // Derecha
-            {
-                if (DrawingArea.ActualWidth / 2 - (transform.TranslateX + deltaTranslationX) +
-                    imageWidth * (0.5 - transform.ScaleX) > 0)
-                {
-                    transform.TranslateX += deltaTranslationX;
-                }
-                else
-                {
-                    transform.TranslateX = imageWidth * (0.5 - transform.ScaleX) + DrawingArea.ActualWidth / 2;
-                }
-            }
-            
-        
-            if (deltaTranslationY < 0) // Arriba
-            {
-                if (DrawingArea.ActualHeight / 2 + (transform.TranslateY + deltaTranslationY) - imageHeight / 2 > 0)
-                {
-                    transform.TranslateY += deltaTranslationY;
-                }
-                else
-                {
-                    transform.TranslateY = imageHeight / 2 - DrawingArea.ActualHeight / 2;
-                }
-            }
-            else // Abajo
-            {
-                if (DrawingArea.ActualHeight / 2 - (transform.TranslateY + deltaTranslationY) +
-                    imageHeight * (0.5 - transform.ScaleY) > 0)
-                {
-                    transform.TranslateY += deltaTranslationY;
-                }
-                else
-                {
-                    transform.TranslateY = imageHeight * (0.5 - transform.ScaleY) + DrawingArea.ActualHeight / 2;
-                }
-            }
+            transform.ScaleX = startScale * e.Cumulative.Scale;
+            transform.ScaleY = startScale * e.Cumulative.Scale;
+            transform.TranslateX = startTranslation.X + e.Cumulative.Translation.X;
+            transform.TranslateY = startTranslation.Y + e.Cumulative.Translation.Y;
+            transform.Rotation = startRotation + e.Cumulative.Rotation;
         }
 
         private void ImageOnManipulationCompleted(object sender, ManipulationCompletedRoutedEventArgs e)
